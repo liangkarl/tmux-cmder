61e540600e80f29e37024cdf6b8620cd4c3284d5
Karl Liang <liang.karl@outlook.com>
2025-05-30 09:46:44 -0600

    feat: add files

---
+++ scripts/menu.bash
@@ -0,0 +1,228 @@
+#!/usr/bin/env bash
+
+__m_height=${__m_height:-10}
+__m_opts=("${__m_opts[@]}")
+__m_backend=${__m_backend:-fzf}
+
+vars=(
+	"__m_title"
+	"__m_prompt"
+	"__m_defopt"
+	"__m_backend"
+	"__m_callback"
+	"__m_view"
+	"__m_width"
+	"__m_height"
+	"__m_ans_idx"
+	"__m_ans_opt"
+	"__m_fzf_cmd"
+)
+
+for f in ${vars[@]}; do
+	eval "function ${f//__m_/menu.}() {
+		if [[ \$# -eq 0 ]]; then
+			echo \"\$$f\"
+			return
+		fi
+
+		$f="\$1"
+	}"
+done
+unset vars f
+
+# TODO:
+# 1. Add multiple name space, making reinit menu unnecessary.
+# 2. Use eval to dynamically create the vars of multiple name space.
+
+menu.opts() {
+	if [[ $# -eq 0 ]]; then
+		echo "${__m_opts[@]}"
+		return
+	fi
+
+	set -- "$@"
+	__m_opts=("$@")
+}
+
+menu.add_opt() {
+	__m_opts+=("$1")
+}
+
+menu.result() {
+	menu.ans_idx
+	menu.ans_opt
+}
+
+menu.reset() {
+	unset __m_defopt
+	unset __m_title
+	unset __m_prompt
+	unset __m_input
+	unset __m_callback
+	unset __m_view
+	unset __m_width
+	unset __m_ans_idx
+	unset __m_ans_opt
+}
+
+menu.view_only() {
+	__m_view=y
+	__m_backend='legacy'
+}
+
+menu.run() {
+	local index input idx
+
+	if [[ -z "$(menu.opts)" ]]; then
+		echo "No opts exist." >&2
+		return 1
+	fi
+
+	if [[ -n "$(menu.view)" ]]; then
+		menu.view
+		return
+	fi
+
+	idx="$(menu.$(menu.backend))"
+	if [[ -z "$idx" ]]; then
+		menu.ans_idx "-1"
+		menu.ans_opt ""
+		return 1
+	fi
+
+	menu.ans_idx "$idx"
+	menu.ans_opt "${__m_opts[$idx]}"
+
+	if [[ -n "$(menu.callback)" ]]; then
+		eval "$(menu.callback) $(menu.ans_idx) $(menu.ans_opt)"
+	fi
+}
+
+menu.get() {
+	local str
+
+	case "$1" in
+		fzf)
+			menu.fzf_cmd
+			;;
+		*)
+			;;
+	esac
+}
+
+__menu_fzf() {
+	local fzf
+
+	fzf="$(which fzf) --height=~${__m_height} --cycle"
+
+	if [[ -n "$__m_defopt" ]]; then
+		fzf+=" --query=${__m_opts[$__m_defopt]}"
+	fi
+
+	if [[ -n "$__m_title" ]]; then
+		fzf+=" --header='${__m_title}'"
+	fi
+
+	if [[ -n "$__m_prompt" ]]; then
+		fzf+=" --prompt='${__m_prompt}: '"
+	fi
+
+	echo "$fzf"
+}
+
+menu.fzf() {
+	local fzf opt i
+
+	fzf="$(__menu_fzf)"
+
+	opt=$(eval "echo ${__m_opts[@]// /%} | sed -e 's/ /\n/g' -e 's/%/ /g' | $fzf")
+
+	for i in "${!__m_opts[@]}"; do
+		if [[ "${__m_opts[$i]}" == "$opt" ]]; then
+			echo "$i"
+			return
+		fi
+	done
+}
+
+menu.fzf-tmux() {
+	local fzf opt i
+
+	fzf=("$(which fzf-tmux)" "-p" "-h ${__m_height}" "--")
+
+	if [[ -n "$__m_defopt" ]]; then
+		fzf+=("--query=${__m_opts[$__m_defopt]}")
+	fi
+
+	if [[ -n "$__m_title" ]]; then
+		fzf+=("--header=${__m_title}")
+	fi
+
+	if [[ -n "$__m_prompt" ]]; then
+		fzf+=("--prompt=${__m_prompt}: ")
+	fi
+
+	opt=$(eval "echo ${__m_opts[@]// /%} | sed -e 's/ /\n/g' -e 's/%/ /g' | ${fzf[*]}")
+
+	for i in "${!__m_opts[@]}"; do
+		if [[ "${__m_opts[$i]}" == "$opt" ]]; then
+			echo "$i"
+			return
+		fi
+	done
+}
+
+menu.fzy() {
+	local fzy opt
+
+	fzy=("fzy" "--lines=${__m_height}")
+
+	if [[ -n "$__m_defopt" ]]; then
+		fzy+=("-q ${__m_opts[$__m_defopt]}")
+	fi
+
+	# output nothing is no title available
+	printf "$__m_title"
+
+	opt=$(eval "echo ${__m_opts[@]// /%} | sed -e 's/ /\n/g' -e 's/%/ /g' | ${fzy[*]}")
+
+	for i in "${!__m_opts[@]}"; do
+		if [[ "${__m_opts[$i]}" == "$opt" ]]; then
+			echo "$i"
+			return
+		fi
+	done
+}
+
+menu._view() {
+	local i opt
+
+	printf -- "$__m_title" > /dev/tty
+
+	for opt in "${__m_opts[@]}"; do
+		echo "$((i++))) $opt"
+	done > /dev/tty
+}
+
+menu.legacy() {
+	local item opts ans prompt
+
+	menu._view
+
+	if [[ -n "$__m_defopt" ]]; then
+		prompt="$__m_prompt ($__m_defopt):"
+	else
+		prompt="$__m_prompt:"
+	fi
+	while :; do
+		read -p "${prompt} " ans
+		ans=${ans:-$__m_defopt}
+
+		if [[ -n "$ans" ]] && (( ans >= 0 && ans < ${#__m_opts[@]})); then
+			break
+		fi 2>&-
+	done
+
+	echo "$ans"
+}
+menu.reset
---
+++ scripts/run.bash
@@ -0,0 +1,421 @@
+#!/usr/bin/env bash
+
+# basic information
+session="$(tmux display-message -p '#S')"
+window="$(tmux display-message -p '#W')"
+client="$(tmux display-message -p '#{client_name}')"
+pane="$(tmux display-message -p '#{pane_id}')"
+
+# finder='fzf-tmux -p 40%,40% -- --reverse'
+
+source menu.bash
+
+main_menu=("clipboard" "keys" "tools" "plugin" "session" "window" "pane")
+session_menu=("new" "kill" "switch" "detach" "rename" "info")
+win_menu=("swap" "break" "kill" "switch" "move" "rename" "link" "info")
+pane_menu=("rename" "swap" "break" "kill" "switch" "join" "layout" "resize" "info")
+cb_ans=
+
+if ! type -t fzf; then
+    echo "no 'fzf' found!" >&2
+    return 1
+fi &> /dev/null
+
+input_box() {
+    echo | fzf --print-query --prompt "$*: " | head -n 1
+}
+
+list_win_all() {
+    tmux list-windows -a -F "#S:#I - #W"
+}
+
+list_win_session() {
+    tmux list-windows -F "[#S] #I:#W"
+}
+
+list_pane_all() {
+    tmux list-panes -a -F "[#S] #I:#W/#P:#T"
+}
+
+list_pane_window() {
+    tmux list-panes -F "[#S] #I:#W/#P:#T"
+}
+
+session_info() {
+    local list
+    list="
+session_activity
+session_alerts
+session_attached
+session_attached_list
+session_created
+session_format
+session_group
+session_group_attached
+session_group_attached_list
+session_group_list
+session_group_many_attached
+session_group_size
+session_grouped
+session_id
+session_last_attached
+session_many_attached
+session_marked
+session_name
+session_path
+session_stack
+session_windows
+    "
+    for var in $list; do
+        printf "%-30s: %s\n" "$var" "$(tmux display-message -p "#{$var}")"
+    done
+}
+
+win_info() {
+    local list
+    list="window_active
+window_active_clients
+window_active_clients_list
+window_active_sessions
+window_active_sessions_list
+window_activity
+window_activity_flag
+window_bell_flag
+window_bigger
+window_cell_height
+window_cell_width
+window_end_flag
+window_flags
+window_format
+window_height
+window_id
+window_index
+window_last_flag
+window_layout
+window_linked
+window_linked_sessions
+window_linked_sessions_list
+window_marked_flag
+window_name
+window_offset_x
+window_offset_y
+window_panes
+window_raw_flags
+window_silence_flag
+window_stack_index
+window_start_flag
+window_visible_layout
+window_width
+window_zoomed_flag
+wrap_flag
+"
+    for var in $list; do
+        printf "%-30s: %s\n" "$var" "$(tmux display-message -p "#{$var}")"
+    done
+}
+
+pane_info() {
+    local list
+    list="pane_active
+pane_at_bottom
+pane_at_left
+pane_at_right
+pane_at_top
+pane_bg
+pane_bottom
+pane_current_command
+pane_current_path
+pane_dead
+pane_dead_signal
+pane_dead_status
+pane_dead_time
+pane_fg
+pane_format
+pane_height
+pane_id
+pane_in_mode
+pane_index
+pane_input_off
+pane_last
+pane_left
+pane_marked
+pane_marked_set
+pane_mode
+pane_path
+pane_pid
+pane_pipe
+pane_right
+pane_search_string
+pane_start_command
+pane_start_path
+pane_synchronized
+pane_tabs
+pane_title
+pane_top
+pane_tty
+pane_unseen_changes
+pane_width
+    "
+    for var in $list; do
+        printf "%-30s: %s\n" "$var" "$(tmux display-message -p "#{$var}")"
+    done
+}
+
+# handle_main [state1] [state2]
+handle_main() {
+    local ans idx state
+
+    if [[ -n "$1" ]]; then
+        state=$1
+        shift
+        handle_${state} "$@"
+        return
+    fi
+
+    state="handle_main"
+    while :; do
+        menu.height 15
+        menu.opts "${main_menu[@]}"
+        menu.add_opt "exit"
+        menu.run
+        ans=$(menu.ans_opt)
+        idx=$(menu.ans_idx)
+
+        if [[ -z "$idx" || -z "$ans" ]]; then
+            return 1
+        fi
+
+        if [[ "$ans" == "exit" ]]; then
+            exit
+        fi
+
+        handle_${ans}
+    done
+}
+
+run_win_cmd() {
+    local ans idx state
+    local src dst
+    local sym opt
+
+    declare -A sym opt
+    sym=([swap]="<->"
+         [link]="~>"
+         [move]="->")
+    opt=([break]="break-pane -P"
+         [kill]="kill-window"
+         [switch]="select-window"
+         [rename]="rename-window"
+         [new]="new-window -n")
+
+    ans="$1"
+    case "$ans" in
+        rename | new)
+            name=$(input_box 'Enter new window title')
+            # current window
+            tmux ${opt[$ans]} "$name"
+            ;;
+        swap | move | link)
+            src=$(list_win_all | fzf)
+            [[ -z "$src" ]] && return
+
+            dst=$(list_win_all | sed -e "s/^/$src ${sym[$ans]} /" | fzf | sed -e "s/^$src ${sym[$ans]} //")
+            [[ -z "$dst" ]] && return
+
+            src=${src%%-*}
+            dst=${dst%%-*}
+            tmux ${ans}-window -s $src -t $dst
+            ;;
+        break | kill | switch)
+            src=$(list_win_all | fzf)
+            [[ -z "$src" ]] && return
+
+            src=${src%%-*}
+            tmux ${opt[$ans]} -t $src
+            ;;
+        info)
+            win_info | fzf
+            ;;
+        exit) return;;
+        *) return 1;;
+    esac
+}
+
+handle_window() {
+    local ans idx state op
+    local src dst
+
+    if [[ -n "$1" ]]; then
+        op=$1
+        run_win_cmd $op
+        exit
+    fi
+
+    state="handle_window"
+    menu.height 15
+    menu.opts "${win_menu[@]}"
+    menu.add_opt "exit"
+    menu.add_opt "back"
+    while :; do
+        menu.run
+        ans=$(menu.ans_opt)
+        idx=$(menu.ans_idx)
+
+        if [[ -z "$idx" ]]; then
+            return 1
+        fi
+
+        run_win_cmd $ans && exit
+    done
+}
+
+run_pane_cmd() {
+    local ans idx state
+    local src dst
+    local sym opt
+
+    declare -A sym opt
+    sym=([swap]="<->"
+         [link]="~>"
+         [move]="->")
+    opt=([break]="break-pane -P"
+         [kill]="kill-window"
+         [switch]="select-window"
+         [rename]="rename-window"
+         [new]="new-window -n")
+
+    ans="$1"
+    case "$ans" in
+        rename | new)
+            name=$(input_box 'Enter new window title')
+            # current window
+            tmux ${opt[$ans]} "$name"
+            ;;
+        swap | move | link)
+            src=$(list_win_all | fzf)
+            [[ -z "$src" ]] && return
+
+            dst=$(list_win_all | sed -e "s/^/$src ${sym[$ans]} /" | fzf | sed -e "s/^$src ${sym[$ans]} //")
+            [[ -z "$dst" ]] && return
+
+            src=${src%%-*}
+            dst=${dst%%-*}
+            tmux ${ans}-window -s $src -t $dst
+            ;;
+        break | kill | switch)
+            src=$(list_win_all | fzf)
+            [[ -z "$src" ]] && return
+
+            src=${src%%-*}
+            tmux ${opt[$ans]} -t $src
+            ;;
+        info)
+            win_info | fzf
+            ;;
+        exit) return;;
+        *) return 1;;
+    esac
+}
+
+handle_pane() {
+    local ans idx state
+
+    state="handle_pane"
+    menu.height 15
+    menu.opts "${pane_menu[@]}"
+    menu.add_opt "exit"
+    menu.add_opt "back"
+    while :; do
+        menu.run
+        ans=$(menu.ans_opt)
+        idx=$(menu.ans_idx)
+
+        if [[ -z "$idx" ]]; then
+            return 1
+        fi
+
+        run_win_cmd $ans && exit
+    done
+}
+
+run_session_cmd() {
+    local ans idx state
+    local src dst
+    local sym opt
+
+    declare -A sym opt
+    sym=([swap]="<->"
+         [link]="~>"
+         [move]="->")
+    opt=([break]="break-pane -P"
+         [kill]="kill-window"
+         [switch]="select-window"
+         [rename]="rename-window"
+         [new]="new-window -n")
+
+    ans="$1"
+    case "$ans" in
+        rename | new)
+            name=$(input_box 'Enter new window title')
+            # current window
+            tmux ${opt[$ans]} "$name"
+            ;;
+        swap | move | link)
+            src=$(list_win_all | fzf)
+            [[ -z "$src" ]] && return
+
+            dst=$(list_win_all | sed -e "s/^/$src ${sym[$ans]} /" | fzf | sed -e "s/^$src ${sym[$ans]} //")
+            [[ -z "$dst" ]] && return
+
+            src=${src%%-*}
+            dst=${dst%%-*}
+            tmux ${ans}-window -s $src -t $dst
+            ;;
+        break | kill | switch)
+            src=$(list_win_all | fzf)
+            [[ -z "$src" ]] && return
+
+            src=${src%%-*}
+            tmux ${opt[$ans]} -t $src
+            ;;
+        info)
+            win_info | fzf
+            ;;
+        exit) return;;
+        *) return 1;;
+    esac
+}
+
+handle_session() {
+    local ans idx state
+
+    state="handle_session"
+    while :; do
+        menu.height 15
+        menu.opts "${session_menu[@]}"
+        menu.add_opt "exit"
+        menu.add_opt "back"
+        menu.run
+        ans=$(menu.ans_opt)
+        idx=$(menu.ans_idx)
+
+        case "$ans" in
+            rename)
+                name=$(input_box 'Enter new pane title')
+                ans=cancel
+                tmux select-pane -T "$name" && ans=exit
+                ;;
+            info)
+                ans=cancel
+                session_info | fzf
+                ans=exit
+                ;;
+            exit) exit;;
+            *|back) return;;
+        esac
+    done
+}
+
+menu.backend "fzf"
+# state='handle_main'
+handle_main "$@"
